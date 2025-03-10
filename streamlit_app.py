63bee665578cb4642761e13c0d63eed0d60420e0
jmazcunan <77403471+jmazcunan@users.noreply.github.com>
2025-03-10 18:10:19 +0000

    add expander

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,10 @@ import streamlit as st
 
 st.title("Tallat  -  Fallas 2025 🔥")
 
-cols = st.columns(3)
 
-with st.expander("Pulseras"):
+
+with st.expander("Pulseras", expanded=True):
+    cols = st.columns(3)
     cols[0].image("static/pulseras1.jpg")
     cols[1].image("static/pulseras2.jpg")
     cols[2].image("static/pulseras3.jpg")
