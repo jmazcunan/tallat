fe1f07fa4a54d3d927aef8acb53bbb7e5b9c73ca
jmazcunan <77403471+jmazcunan@users.noreply.github.com>
2025-03-10 18:07:26 +0000

    add content

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,15 @@
 import streamlit as st
 
-st.title("Tallat")
+st.title("Tallat  -  Fallas 2025 🔥")
+
+cols = st.columns(3)
+
+with st.expander("Pulseras"):
+    cols[0].image("static/pulseras1.jpg")
+    cols[1].image("static/pulseras2.jpg")
+    cols[2].image("static/pulseras3.jpg")
+
+st.subheader("Pines")
+    
+
+st.subheader("Broches")
